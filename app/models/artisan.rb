5a09861622d1eb536a0e9f3cb57562560a7a2be5
Tamara Dowis <tamara.dowis@gmail.com>
2024-12-25 13:09:05 -0600

    Enhance Artisan model with secure password, product associations, and email validations

--- app/models/artisan.rb
+++ app/models/artisan.rb
@@ -1,3 +1,9 @@
 class Artisan < ApplicationRecord
   belongs_to :admin
+
+  has_many :products, dependent: :destroy
+  has_secure_password
+
+  validates :store_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must be a valid email address' }
 end
