afa6f9f3e70dfbc2a05ec5ec0fad8019ff043f4d
Tamara Dowis <tamara.dowis@gmail.com>
2024-12-25 17:18:28 -0600

    Add sessions controller, views, and helper for admin authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+    # Render login form
+  end
+
+  def create
+    admin = Admin.find_by(email: params[:email])
+    if admin&.authenticate(params[:password])
+      session[:admin_id] = admin.id
+      redirect_to admin_path(admin), notice: 'Logged in successfully'
+    else
+      flash.now[:alert] = 'Invalid email or password'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:admin_id] = nil
+    redirect_to root_path, notice: 'Logged out successfully'
+  end
+end
