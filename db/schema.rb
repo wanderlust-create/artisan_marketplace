4e71875384a1575fbf17b516b0887bb18931bbc9
Tamara Dowis <tamara.dowis@gmail.com>
2024-12-25 16:00:35 -0600

    Update schema.rb to reflect new database version and formatting changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,93 +10,93 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_241_225_213_037) do
-  create_table 'admins', force: :cascade do |t|
-    t.string 'email'
-    t.string 'password_digest'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_admins_on_email', unique: true
+ActiveRecord::Schema[7.0].define(version: 2024_12_25_213037) do
+  create_table "admins", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
   end
 
-  create_table 'artisans', force: :cascade do |t|
-    t.string 'store_name'
-    t.string 'email'
-    t.string 'password_digest'
-    t.integer 'admin_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['admin_id'], name: 'index_artisans_on_admin_id'
-    t.index ['email'], name: 'index_artisans_on_email', unique: true
+  create_table "artisans", force: :cascade do |t|
+    t.string "store_name"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "admin_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["admin_id"], name: "index_artisans_on_admin_id"
+    t.index ["email"], name: "index_artisans_on_email", unique: true
   end
 
-  create_table 'customers', force: :cascade do |t|
-    t.string 'first_name'
-    t.string 'last_name'
-    t.string 'email'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_customers_on_email', unique: true
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
   end
 
-  create_table 'invoice_items', force: :cascade do |t|
-    t.integer 'invoice_id', null: false
-    t.integer 'product_id', null: false
-    t.integer 'quantity'
-    t.decimal 'unit_price'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'status', default: 0, null: false
-    t.index ['invoice_id'], name: 'index_invoice_items_on_invoice_id'
-    t.index ['product_id'], name: 'index_invoice_items_on_product_id'
+  create_table "invoice_items", force: :cascade do |t|
+    t.integer "invoice_id", null: false
+    t.integer "product_id", null: false
+    t.integer "quantity"
+    t.decimal "unit_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "status", default: 0, null: false
+    t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
+    t.index ["product_id"], name: "index_invoice_items_on_product_id"
   end
 
-  create_table 'invoices', force: :cascade do |t|
-    t.integer 'customer_id', null: false
-    t.integer 'status', default: 0, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['customer_id'], name: 'index_invoices_on_customer_id'
+  create_table "invoices", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_invoices_on_customer_id"
   end
 
-  create_table 'products', force: :cascade do |t|
-    t.string 'name'
-    t.text 'description'
-    t.decimal 'price'
-    t.integer 'stock'
-    t.integer 'artisan_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['artisan_id'], name: 'index_products_on_artisan_id'
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.integer "stock"
+    t.integer "artisan_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artisan_id"], name: "index_products_on_artisan_id"
   end
 
-  create_table 'reviews', force: :cascade do |t|
-    t.integer 'rating'
-    t.text 'text'
-    t.integer 'product_id', null: false
-    t.integer 'customer_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['customer_id'], name: 'index_reviews_on_customer_id'
-    t.index ['product_id'], name: 'index_reviews_on_product_id'
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "text"
+    t.integer "product_id", null: false
+    t.integer "customer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_reviews_on_customer_id"
+    t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
-  create_table 'transactions', force: :cascade do |t|
-    t.integer 'invoice_id', null: false
-    t.string 'credit_card_number'
-    t.string 'credit_card_expiration_date'
-    t.integer 'status'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['invoice_id'], name: 'index_transactions_on_invoice_id'
+  create_table "transactions", force: :cascade do |t|
+    t.integer "invoice_id", null: false
+    t.string "credit_card_number"
+    t.string "credit_card_expiration_date"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
-  add_foreign_key 'artisans', 'admins'
-  add_foreign_key 'invoice_items', 'invoices'
-  add_foreign_key 'invoice_items', 'products'
-  add_foreign_key 'invoices', 'customers'
-  add_foreign_key 'products', 'artisans'
-  add_foreign_key 'reviews', 'customers'
-  add_foreign_key 'reviews', 'products'
-  add_foreign_key 'transactions', 'invoices'
+  add_foreign_key "artisans", "admins"
+  add_foreign_key "invoice_items", "invoices"
+  add_foreign_key "invoice_items", "products"
+  add_foreign_key "invoices", "customers"
+  add_foreign_key "products", "artisans"
+  add_foreign_key "reviews", "customers"
+  add_foreign_key "reviews", "products"
+  add_foreign_key "transactions", "invoices"
 end
