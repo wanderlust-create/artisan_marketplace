c85db671b9ba41bf1cbd4bb1b8125c2158aedd1d
Tamara Dowis <tamara.dowis@gmail.com>
2024-12-25 13:08:56 -0600

    Enhance Admin model with secure password, associations, and email validations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,8 @@
+
 class Admin < ApplicationRecord
+  has_secure_password
+  has_many :artisans, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must be a valid email address' }
 end
+
