3802825569390280cf39fb9b601cf48339541862
Tamara Dowis <tamara.dowis@gmail.com>
2024-12-25 16:00:46 -0600

    Change invoice status column type from string to integer with up/down migration methods

--- db/migrate/20241225213037_change_status_to_integer_in_invoices.rb
+++ db/migrate/20241225213037_change_status_to_integer_in_invoices.rb
@@ -1,5 +1,9 @@
 class ChangeStatusToIntegerInInvoices < ActiveRecord::Migration[7.0]
-  def change
+  def up
     change_column :invoices, :status, :integer, using: 'status::integer', null: false, default: 0
+  end
+
+  def down
+    change_column :invoices, :status, :string
   end
 end
