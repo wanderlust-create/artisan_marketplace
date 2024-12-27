3d6225fe3267c4ad28bfbec0bdba98cd7e995ecd
Tamara Dowis <tamara.dowis@gmail.com>
2024-12-26 21:05:42 -0600

    Add feature spec for admin deleting artisan

---
+++ spec/features/admin_deletes_artisan_spec.rb
@@ -0,0 +1,36 @@
+RSpec.feature 'AdminDeletesArtisan', type: :feature do
+  let(:admin) { FactoryBot.create(:admin, email: 'admin@example.com', password: 'password') }
+
+  before do
+    FactoryBot.create(:artisan, admin: admin, store_name: 'Artisan Wonders')
+    login_as_admin(admin)
+  end
+
+  scenario 'Admin visits the artisans list page' do
+    visit admin_path(admin)
+    expect(page).to have_content('Artisan Wonders')
+  end
+
+  scenario 'Admin deletes an artisan with Turbo confirmation', :js do
+    visit admin_path(admin)
+
+    expect(page).to have_content('Artisan Wonders')
+
+    # Handle Turbo's confirmation modal
+    accept_confirm do
+      click_link 'Delete Artisan Data'
+    end
+
+    expect(page).to have_content('Artisan was successfully deleted.')
+    expect(page).not_to have_content('Artisan Wonders')
+  end
+
+  private
+
+  def login_as_admin(admin)
+    visit '/login'
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: admin.password
+    click_button 'Login'
+  end
+end
