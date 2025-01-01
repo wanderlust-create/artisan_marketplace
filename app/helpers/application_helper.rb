8f68179973029e19cac223b4cba39850fd3621f0
Tamara Dowis <tamara.dowis@gmail.com>
2024-12-31 20:33:45 -0600

    Add dashboard path helper method for user roles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def dashboard_path_for(user)
+    return root_path unless user
+
+    if user.is_a?(Admin)
+      # TODO: Update to super_admin_dashboard_path after it has been built
+      dashboard_admin_path(user.id)
+    elsif user.is_a?(Artisan)
+      dashboard_artisan_path(user.id)
+    else
+      root_path
+    end
+  end
 end
