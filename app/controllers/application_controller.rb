541be2e6d2a2599f50eb5707596863d728232c6b
Tamara Dowis <tamara.dowis@gmail.com>
2025-01-02 14:15:25 -0600

    Move dashboard_path_for method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
-
   def current_user
     @current_user ||= Admin.find_by(id: session[:user_id]) || Artisan.find_by(id: session[:user_id])
   end
+
+  def dashboard_path_for(user)
+    if user.is_a?(Admin)
+      # Use super_admin_dashboard_path once implemented
+      # return super_admin_dashboard_path if user.super_admin?
+      return dashboard_admin_path(user.id)
+    elsif user.is_a?(Artisan)
+      return dashboard_artisan_path(user.id)
+    end
+
+    root_path # Fallback for unexpected cases
+  end
+
+  helper_method :current_user, :dashboard_path_for # Makes this method available in views
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
 module ApplicationHelper
-  def dashboard_path_for(user)
-    if user.is_a?(Admin)
-      # Use super_admin_dashboard_path once implemented
-      # return super_admin_dashboard_path if user.super_admin?
-      return dashboard_admin_path(user.id)
-    elsif user.is_a?(Artisan)
-      return dashboard_artisan_path(user.id)
-    end
-
-    root_path # Fallback for unexpected cases
-  end
 end
