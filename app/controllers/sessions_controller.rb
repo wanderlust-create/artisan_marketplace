eebdb661aeba13623ad7d37aa7e8c61e7ed19e97
Tamara Dowis <tamara.dowis@gmail.com>
2025-01-02 13:57:39 -0600

    Abstract dashboard_path_for func to the ApplicationHelper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -31,18 +31,6 @@ class SessionsController < ApplicationController
     session[:role] = user.role if user.is_a?(Admin) # Save role if user is an admin
   end
 
-  def dashboard_path_for(user)
-    if user.is_a?(Admin)
-      # TODO: Implement super_admin_dashboard_path and use it here
-      # return super_admin_dashboard_path if user.super_admin?
-      return dashboard_admin_path(user.id)
-    elsif user.is_a?(Artisan)
-      return dashboard_artisan_path(user.id)
-    end
-
-    root_path
-  end
-
   def handle_login_failure
     flash.now[:alert] = 'Invalid email or password. Please try again.'
     render :new
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,13 @@
 module ApplicationHelper
   def dashboard_path_for(user)
-    return root_path unless user
-
     if user.is_a?(Admin)
-      # TODO: Update to super_admin_dashboard_path after it has been built
-      dashboard_admin_path(user.id)
+      # Use super_admin_dashboard_path once implemented
+      # return super_admin_dashboard_path if user.super_admin?
+      return dashboard_admin_path(user.id)
     elsif user.is_a?(Artisan)
-      dashboard_artisan_path(user.id)
-    else
-      root_path
+      return dashboard_artisan_path(user.id)
     end
+
+    root_path # Fallback for unexpected cases
   end
 end
