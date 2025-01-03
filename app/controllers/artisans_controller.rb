fa197ddd273928fced218b1e67c7125f1c30dd54
Tamara Dowis <tamara.dowis@gmail.com>
2025-01-02 19:29:02 -0600

    Add authorization and login requirements to ArtisansController actions

--- app/controllers/artisans_controller.rb
+++ app/controllers/artisans_controller.rb
@@ -1,6 +1,9 @@
 class ArtisansController < ApplicationController
+  before_action :require_login
   before_action :set_admin, only: %i[index show new create edit update destroy]
-  before_action :set_artisan, only: %i[show update]
+  before_action :set_artisan, only: %i[show update destroy]
+  before_action :authorize_artisan_access, only: %i[edit update]
+  helper_method :can_edit_artisan?, :can_view_artisan?, :can_delete_artisan?
 
   def index
     @artisans = @admin.artisans
@@ -25,12 +28,13 @@ class ArtisansController < ApplicationController
   end
 
   def edit
-    @artisan = @admin.artisans.find(params[:id])
+    # @artisan is already set via before_action
   end
 
   def update
     if @artisan.update(artisan_params)
       handle_status_change
+      @artisan.save
       redirect_to artisan_path(@artisan), notice: flash[:notice]
     else
       flash.now[:alert] = 'There was an error updating the artisan.'
@@ -39,9 +43,9 @@ class ArtisansController < ApplicationController
   end
 
   def destroy
-    @artisan = @admin.artisans.find(params[:id])
-    store_name = @artisan.store_name # Fetch the store name before deletion
+    store_name = @artisan.store_name
     if @artisan.destroy
+
       redirect_to dashboard_admin_path(@admin), notice: "Artisan #{store_name} and all associated data were successfully deleted."
     else
       redirect_to dashboard_admin_path(@admin), alert: "Failed to delete Artisan #{store_name}."
@@ -53,7 +57,32 @@ class ArtisansController < ApplicationController
     @products = @artisan.products
   end
 
+  def can_edit_artisan?
+    # binding.pry
+    current_user == @artisan || current_user == @artisan.admin || current_user.super_admin?
+  end
+
+  def can_delete_artisan?
+    if current_user.is_a?(Admin)
+      current_user.super_admin? || current_user == @artisan.admin
+    elsif current_user.is_a?(Artisan)
+      false # Artisans should not have permission to delete
+    else
+      false # Default to no permission
+    end
+  end
+
+  def can_view_artisan?
+    current_user.is_a?(Admin) || current_user == @artisan
+  end
+
   private
+
+  def require_login
+    return if current_user
+
+    redirect_to auth_login_path, alert: 'You must be logged in to access this page.'
+  end
 
   def set_admin
     if params[:admin_id]
@@ -67,6 +96,13 @@ class ArtisansController < ApplicationController
     end
   end
 
+  def authorize_artisan_access
+    return if can_edit_artisan?
+
+    flash[:alert] = 'You are not authorized to perform this action.'
+    redirect_to artisan_path(@artisan)
+  end
+
   def set_artisan
     @artisan = Artisan.find(params[:id])
   end
@@ -76,10 +112,8 @@ class ArtisansController < ApplicationController
   end
 
   def handle_status_change
-    flash[:notice] = if artisan_params[:active] == 'false'
-                       'Artisan has been successfully deactivated.'
-                     elsif artisan_params[:active] == 'true'
-                       'Artisan has been successfully reactivated.'
+    flash[:notice] = if artisan_params[:active].present?
+                       "Artisan has been successfully #{artisan_params[:active] == 'true' ? 'reactivated' : 'deactivated'}."
                      else
                        'Artisan details have been successfully updated.'
                      end
