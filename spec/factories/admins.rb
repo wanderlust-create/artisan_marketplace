8b6963bbda2ec888c3d4787cc3855b5bf822a31c
Tamara Dowis <tamara.dowis@gmail.com>
2025-01-03 17:44:52 -0600

    Add role attribute to admin factory with super_admin trait

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -2,5 +2,11 @@ FactoryBot.define do
   factory :admin do
     email { Faker::Internet.email }
     password { 'password' }
+    role { :regular }
+
+    trait :super_admin do
+      role { :super_admin }
+    end
   end
 end
+
