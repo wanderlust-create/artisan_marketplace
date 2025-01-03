6c687633c630574faa48cdd25d60e73c2e67be8b
Tamara Dowis <tamara.dowis@gmail.com>
2025-01-03 17:43:32 -0600

    Refactor artisan permission methods to consolidate logic and improve readability

--- app/controllers/concerns/artisan_permissions.rb
+++ app/controllers/concerns/artisan_permissions.rb
@@ -2,24 +2,21 @@ module ArtisanPermissions
   extend ActiveSupport::Concern
 
   included do
-    helper_method :can_edit_artisan?, :can_delete_artisan?, :can_view_artisan?
+    helper_method :can_edit_artisan?, :can_manage_artisan?, :can_view_artisan?
   end
 
-  def can_edit_artisan?
-    if current_user.is_a?(Admin)
-      current_user.super_admin? || current_user.id == @artisan.admin_id
-    elsif current_user.is_a?(Artisan)
-      current_user == @artisan
-    else
-      false
-    end
-  end
-
-  def can_delete_artisan?
-    if current_user.is_a?(Admin)
-      current_user.super_admin? || current_user.id == @artisan.admin_id
-    elsif current_user.is_a?(Artisan)
-      false
+  def can_manage_artisan?(action) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+    case action
+    when :edit
+      if current_user.is_a?(Admin)
+        current_user.super_admin? || current_user.id == @artisan.admin_id
+      elsif current_user.is_a?(Artisan)
+        current_user == @artisan
+      else
+        false
+      end
+    when :create, :delete
+      current_user.is_a?(Admin) && (current_user.super_admin? || current_user.id == @artisan.admin_id)
     else
       false
     end
@@ -28,4 +25,5 @@ module ArtisanPermissions
   def can_view_artisan?
     current_user.is_a?(Admin) || current_user == @artisan
   end
+
 end
