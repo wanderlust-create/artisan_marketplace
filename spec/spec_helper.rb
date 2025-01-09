eb8c1bc135f211de52b2957a372e46fa5f7add89
Tamara Dowis <tamara.dowis@gmail.com>
2025-01-08 21:05:09 -0600

    Configure FactoryBot definition file paths in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,11 @@ require 'factory_bot_rails'
 RSpec.configure do |config|
   # Include FactoryBot methods to simplify syntax
   config.include FactoryBot::Syntax::Methods
+
+  # Define the paths for FactoryBot definitions
+  FactoryBot.definition_file_paths = [File.expand_path('factories', __dir__)]
+  FactoryBot.find_definitions
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
