51135b216f36d155e28e5b88770cb02c7550c1bb
Tamara Dowis <tamara.dowis@gmail.com>
2025-01-20 15:40:37 -0600

    Update discount factory to set discount_price less than product.price

--- spec/factories/discount.rb
+++ spec/factories/discount.rb
@@ -1,8 +1,14 @@
 FactoryBot.define do
   factory :discount do
     association :product
-    discount_price { Faker::Commerce.price(range: 1.0..500.0) }
+    discount_price { Faker::Commerce.price(range: 0.01..100.0) } # Default range
     start_date { Time.zone.today }
-    end_date { Time.zone.today + Faker::Number.between(from: 1, to: 30) }
+    end_date { Time.zone.today + 7 }
+    # Ensures the discount price is less than the product price
+    trait :valid_discount do
+      after(:build) do |discount|
+        discount.discount_price = discount.product.price * 0.8 # 20% discount
+      end
+    end
   end
 end
