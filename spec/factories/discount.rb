7e70cd735faf32a84d211e3d0a9355468ea06d61
Tamara Dowis <tamara.dowis@gmail.com>
2025-01-19 23:42:53 -0600

    Update discount factory to use Time.zone.today

--- spec/factories/discount.rb
+++ spec/factories/discount.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :discount do
     association :product
     discount_price { Faker::Commerce.price(range: 1.0..500.0) }
-    start_date { Date.today }
-    end_date { Date.today + Faker::Number.between(from: 1, to: 30) }
+    start_date { Time.zone.today }
+    end_date { Time.zone.today + Faker::Number.between(from: 1, to: 30) }
   end
 end
