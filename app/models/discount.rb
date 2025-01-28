d78a9ebd35d436964861828152a860450421933b
Tamara Dowis <tamara.dowis@gmail.com>
2025-01-27 21:55:55 -0600

    Enhance discount model validations and add scopes for current and upcoming discounts

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,27 +2,33 @@ class Discount < ApplicationRecord
   belongs_to :product
   has_one :artisan, through: :product
 
-  # Ensure discount price is less than or equal to the product price and greater than 0.
-  # Allows nil values when the user enters a percentage discount, which will be calculated in the controller.
+  # Validations
   validates :discount_price, presence: true, numericality: {
     greater_than: 0,
     less_than_or_equal_to: ->(discount) { discount.product&.price || Float::INFINITY }
   }
-  validates :start_date, presence: true
-  validates :end_date, presence: true
+  validates :start_date, :end_date, presence: true
   validate :end_date_after_start_date
+  # validate :prevent_overlapping_discounts, on: :create
 
-  # Scope to retrieve all discounts associated with a specific artisan.
-  # Joins the products and artisans tables to filter discounts by artisan ID.
+  # Scopes
   scope :by_artisan, ->(artisan_id) { joins(product: :artisan).where(products: { artisan_id: artisan_id }) }
+  scope :current_and_upcoming_discounts, -> { where('end_date >= ?', Time.zone.today).order(:start_date) }
 
   private
 
+  # Validates that the end date is after the start date.
   def end_date_after_start_date
-    return if end_date.blank? || start_date.blank?
-
-    return unless start_date >= end_date
+    return unless start_date.present? && end_date.present? && start_date >= end_date
 
     errors.add(:end_date, 'must be after the start date')
+  end
+
+  # Validates that discounts for the same product do not overlap.
+  def prevent_overlapping_discounts
+    overlapping = product.discounts.where.not(id: id).where(
+      'start_date < ? AND end_date > ?', end_date, start_date
+    )
+    errors.add(:base, 'Discount periods cannot overlap') if overlapping.exists?
   end
 end
