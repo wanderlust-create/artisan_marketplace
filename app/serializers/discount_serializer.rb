2bdad2fdaa210a0177d99a3db4eab5506e8633ab
Tamara Dowis <tamara.dowis@gmail.com>
2025-04-02 22:06:38 -0500

    chore: clean up whitespace

--- app/serializers/discount_serializer.rb
+++ app/serializers/discount_serializer.rb
@@ -2,4 +2,3 @@ class DiscountSerializer < ActiveModel::Serializer
   attributes :id, :original_price, :discount_price, :percentage_off,
              :start_date, :end_date, :discount_type
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # Authentication Routes
   scope :auth, as: :auth do
-    get    '/login',  to: 'sessions#new',     as: :login
+    get    '/login',  to: 'sessions#new', as: :login
     post   '/login',  to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy', as: :logout
   end
