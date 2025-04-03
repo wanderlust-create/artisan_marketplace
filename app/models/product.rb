5dd389ef6642768a1fb3bc5f0b14853b0225eb7e
Tamara Dowis <tamara.dowis@gmail.com>
2025-04-02 19:51:32 -0500

    feat: add scope to Product model for active discounts

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,10 @@ class Product < ApplicationRecord
   validates :name, :price, :stock, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  scope :with_active_discounts, lambda {
+    joins(:discounts)
+      .merge(Discount.active_today)
+      .distinct
+  }
 end
